8a5e080d25e316baeb669d17178b362e29523fb1
Mehmet Ali Kırımlı <133754074+iamkirimli@users.noreply.github.com>
2025-10-19 18:27:36 +0300

    Update UcMesaj.java

---
+++ Task-1-HosgeldinMesajlari/UcMesaj.java
@@ -0,0 +1,9 @@
+public class odev1 {
+
+	public static void main(String[] args) {
+		System.out.println("Java'ya Hoş Geldiniz");
+		System.out.println("Bilgisayar Bilimine Hoş Geldiniz");
+		System.out.println("Programlama Eğlencelidir");
+	}
+
+}
