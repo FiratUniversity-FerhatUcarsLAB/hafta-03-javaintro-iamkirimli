1e5261bc8446870ee1a3d30bd171ad9cc630cfc9
Mehmet Ali Kırımlı <133754074+iamkirimli@users.noreply.github.com>
2025-10-19 19:02:05 +0300

    Update IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,18 @@
+public class odev1 {
+
+	public static void main(String[] args) {
+		String ad = "Mehmet Ali";
+		String soyad = "Kırımlı";
+		String ogrNo = "250542027";
+		String bolum = "Yazilim Müh.";
+		System.out.println ("===================");
+		System.out.println ("ÖĞRENCİ BİLGİLERİ");
+		System.out.println ("===================");
+		System.out.println ("Ad : " + ad);
+		System.out.println ("Soyad : " + soyad );
+		System.out.println ("No : " + ogrNo );
+		System.out.println ("Bölüm : " + bolum );
+		System.out.println ("===================");
+	}
+
+}
