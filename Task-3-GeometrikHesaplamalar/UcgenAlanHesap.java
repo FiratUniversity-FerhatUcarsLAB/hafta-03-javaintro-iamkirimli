07079d2910c4255dd2b4e0ee691a5835f00510ed
Mehmet Ali Kırımlı <133754074+iamkirimli@users.noreply.github.com>
2025-10-19 18:58:20 +0300

    Update UcgenAlanHesap.java

---
+++ Task-3-GeometrikHesaplamalar/UcgenAlanHesap.java
@@ -0,0 +1,13 @@
+public class odev1 {
+
+	public static void main(String[] args) {
+		int a = 3;
+		int b = 4;
+		int c = 5;
+		int s = (a+b+c)/2;
+		double alan = Math.sqrt (s*(s-a)*(s-b)*(s-c));
+		System.out.println("Üçgenin Alanı = " + alan);
+		
+	}
+
+}
