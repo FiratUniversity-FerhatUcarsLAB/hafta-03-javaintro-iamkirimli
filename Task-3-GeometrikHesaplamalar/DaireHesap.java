1204cd036d2ee951eeefb16e73e6d56046a3bdcc
Mehmet Ali Kırımlı <133754074+iamkirimli@users.noreply.github.com>
2025-10-19 18:47:10 +0300

    Update DaireHesap.java

--- Task-3-GeometrikHesaplamalar/DaireHesap.java
+++ Task-3-GeometrikHesaplamalar/DaireHesap.java
@@ -5,8 +5,10 @@ public class odev1 {
 		double pi = 3.14;
 		double alan = (yaricap*yaricap)*pi;
 		double cevre = 2*(pi*yaricap);
+		System.out.println("Yarıçap = " + yaricap);
 		System.out.println("Alan = " + alan);
 		System.out.println("Çevre = " + cevre);
 	}
 
 }
+
