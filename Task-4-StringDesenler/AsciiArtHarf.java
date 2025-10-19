7d9dddffb3f0ba81191c3af4d11ba021e076b266
Mehmet Ali Kırımlı <133754074+iamkirimli@users.noreply.github.com>
2025-10-19 19:05:06 +0300

    Update AsciiArtHarf.java

---
+++ Task-4-StringDesenler/AsciiArtHarf.java
@@ -0,0 +1,14 @@
+public class odev1 {
+
+	public static void main(String[] args) {
+		System.out.println("MM        MMM");
+		System.out.println("MMMM     MMMM");
+		System.out.println("MM  MM MM  MM");
+		System.out.println("MM   MMM   MM");
+		System.out.println("MM         MM");
+		System.out.println("MM         MM");
+		System.out.println("MM         MM");
+		System.out.println("MM         MM");
+	}
+
+}
