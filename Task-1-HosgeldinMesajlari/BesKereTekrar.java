30df446e7646671cda5e270fc5e508528b60b383
Mehmet Ali Kırımlı <133754074+iamkirimli@users.noreply.github.com>
2025-10-19 18:28:05 +0300

    Update BesKereTekrar.java

--- Task-1-HosgeldinMesajlari/BesKereTekrar.java
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -2,8 +2,12 @@ public class odev1 {
 
 	public static void main(String[] args) {
 		System.out.println("Java'ya Hoş Geldiniz");
-		System.out.println("Bilgisayar Bilimine Hoş Geldiniz");
-		System.out.println("Programlama Eğlencelidir");
+		System.out.println("Java'ya Hoş Geldiniz");
+		System.out.println("Java'ya Hoş Geldiniz");
+		System.out.println("Java'ya Hoş Geldiniz");
+		System.out.println("Java'ya Hoş Geldiniz");
 	}
 
 }
+
+
