aecc43fd3023ddf90176520bced8c73dc0bac347
Mehmet Ali Kırımlı <133754074+iamkirimli@users.noreply.github.com>
2025-10-20 16:57:41 +0300

    Update HesapOzeti.java

--- Task-4-StringDesenler/HesapOzeti.java
+++ Task-4-StringDesenler/HesapOzeti.java
@@ -1,4 +1,4 @@
-public class odev1 {
+public class gorev4_3 {
 
 	public static void main(String[] args) {
 		String urun1 = "Deterjan";
@@ -12,8 +12,11 @@ public class odev1 {
 		int ekmfiyat = 15;
 		int toplam = (miktar1*detfiyat)+(miktar2*sutfiyat)+(miktar3*ekmfiyat);
 		System.out.println("Ürünler    " + urun1 + " " + urun2 + " " + urun3);
+		System.out.println ("=============================");
 		System.out.println("Miktar        " + miktar1 + "      " + miktar2 + "    " + miktar3);
+		System.out.println ("=============================");
 		System.out.println("Fiyat        " + detfiyat + "    " + sutfiyat + "   " + ekmfiyat);
+		System.out.println ("=============================");
 		System.out.println("");
 		System.out.println("Toplam = " + toplam + "₺");
 	}
