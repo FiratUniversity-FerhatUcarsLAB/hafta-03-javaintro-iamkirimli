0a1e47b350877915f824bfdc21f57ad29fb7bb6d
Mehmet Ali Kırımlı <133754074+iamkirimli@users.noreply.github.com>
2025-10-20 17:22:28 +0300

    Add files via upload

---
+++ Task-1-HosgeldinMesajlari/gorev1_1.java
@@ -0,0 +1,11 @@
+package odev;
+
+public class gorev1_1 {
+
+	public static void main(String[] args) {
+		System.out.println("Java'ya Hoş Geldiniz");
+		System.out.println("Bilgisayar Bilimine Hoş Geldiniz");
+		System.out.println("Programlama Eğlencelidir");
+	}
+
+}
---
+++ Task-1-HosgeldinMesajlari/gorev1_2.java
@@ -0,0 +1,13 @@
+package odev;
+
+public class gorev1_2 {
+
+	public static void main(String[] args) {
+		System.out.println("Java'ya Hoş Geldiniz");
+		System.out.println("Java'ya Hoş Geldiniz");
+		System.out.println("Java'ya Hoş Geldiniz");
+		System.out.println("Java'ya Hoş Geldiniz");
+		System.out.println("Java'ya Hoş Geldiniz");
+	}
+
+}
