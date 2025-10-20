7a84792e9874e169b4c583838dbbece8506c1f29
Mehmet Ali Kırımlı <133754074+iamkirimli@users.noreply.github.com>
2025-10-20 17:23:45 +0300

    Add files via upload

---
+++ Task-3-GeometrikHesaplamalar/gorev3_1.java
@@ -0,0 +1,15 @@
+package odev;
+
+public class gorev3_1 {
+
+	public static void main(String[] args) {
+		double yaricap = 5.5;
+		double pi = 3.14;
+		double alan = (yaricap*yaricap)*pi;
+		double cevre = 2*(pi*yaricap);
+		System.out.println("Yarıçap = " + yaricap);
+		System.out.println("Alan = " + alan);
+		System.out.println("Çevre = " + cevre);
+	}
+
+}
