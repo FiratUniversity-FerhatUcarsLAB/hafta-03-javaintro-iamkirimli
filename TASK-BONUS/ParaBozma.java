5c629696b56ffdda596e93fc24e790c43d47c1e3
Mehmet Ali Kırımlı <133754074+iamkirimli@users.noreply.github.com>
2025-10-20 15:58:46 +0300

    Update ParaBozma.java

--- TASK-BONUS/ParaBozma.java
+++ TASK-BONUS/ParaBozma.java
@@ -1,14 +1,15 @@
 import java.util.Scanner;
 
-public class odev1 {
+public class bonustask_2 {
 
-    public static void main(String[] args) {
-        
-    	Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
     	System.out.print("Miktarı Girin = ");
     	int para = scanner.nextInt();
-    	int yuzluk = para/100;
-    	int yuzlukkalan = para%100;
+    	int ikiyuzluk = para/200;
+    	int ikiyuzlukkalan = para%200;
+    	int yuzluk = ikiyuzlukkalan/100;
+    	int yuzlukkalan = ikiyuzlukkalan%100;
     	int ellilik = yuzlukkalan/50;
     	int ellilikkalan = yuzlukkalan%50;
     	int yirmilik = ellilikkalan/20;
@@ -18,11 +19,14 @@ public class odev1 {
     	int beslik = onlukkalan/5;
     	int beslikkalan = onlukkalan%5;
     	int birlik = beslikkalan/1;
+    	System.out.println("200 = x" + ikiyuzluk);
     	System.out.println("100 = x" + yuzluk);
     	System.out.println("50 = x" + ellilik);
     	System.out.println("20 = x" + yirmilik);
     	System.out.println("10 = x" + onluk);
     	System.out.println("5 = x" + beslik);
     	System.out.println("1 = x" + birlik);
-    }
+	}
+
 }
+
