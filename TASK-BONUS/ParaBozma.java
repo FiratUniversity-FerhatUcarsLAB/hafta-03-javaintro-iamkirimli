4c5060afd6ca5e3d63b98c05c0bea7e1374fbee3
Mehmet Ali Kırımlı <133754074+iamkirimli@users.noreply.github.com>
2025-10-20 16:52:50 +0300

    Update ParaBozma.java

--- TASK-BONUS/ParaBozma.java
+++ TASK-BONUS/ParaBozma.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class bonustask_2 {
+public class bonustask_3 {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
@@ -29,4 +29,5 @@ public class bonustask_2 {
 	}
 
 }
+
 
